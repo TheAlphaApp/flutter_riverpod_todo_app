84174ed823d2e28f941207e3759d56b9f0e471b2
TheAlphaApp <souravsuman636@gmail.com>
2021-11-03 11:05:46 +0530

    screenshots added and unused code removed

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,7 @@
-const String backupPath = '/Android/data/$appName/files/backup.json';
 const String appName = 'com.example.riverpod_flutter_todo_app';
 const String fbUrl = 'https://www.facebook.com/App-Dexon-102102212273649';
 const String githubUrl = 'https://www.github.com/TheAlphaApp';
-const String mailUrl =
-    'mailto:appdexonofficial@gmail.com?subject=Regarding%20My%20Tasks%20app&body=Hello%20App%20Dexon';
+const String mailUrl = 'mailto:mail@example.com?subject=&body=';
 const String sharedPrefTodoListKey = 'SharedData';
 const String sharedDarkModeKey = 'isDarkModeEnabled';
 const String emptyJsonStringData = '{"data": []}';
