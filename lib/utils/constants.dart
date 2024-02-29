83946b9853c642148979e837317d6b4a8d9b223a
TheAlphaApp <souravsuman636@gmail.com>
2024-02-29 23:59:51 +0530

    updated email

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 const String appName = 'com.example.riverpod_flutter_todo_app';
 const String fbUrl = 'https://www.facebook.com/App-Dexon-102102212273649';
 const String githubUrl = 'https://www.github.com/TheAlphaApp';
-const String mailUrl = 'mailto:mail@example.com?subject=&body=';
+const String mailUrl = 'mailto:appdexonofficial@gmail.com?subject="regarding $appName&body=';
 const String sharedPrefTodoListKey = 'SharedData';
 const String sharedDarkModeKey = 'isDarkModeEnabled';
 const String emptyJsonStringData = '{"data": []}';
